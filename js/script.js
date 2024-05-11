f9c6c855e522f0cc058982cad3579bf6496c9780
NesStudio <nendisetiawan10@gmail.com>
2024-05-11 17:06:18 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,30 @@
+
+// popup
+    document.getElementById("view").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "flex"
+})
+
+    document.querySelector(".btn").addEventListener("click", function(){
+    document.querySelector(".popup").style.display = "none"
+})
+
+
+    document.getElementById("view2").addEventListener("click", function(){
+    document.querySelector(".popup2").style.display = "flex"
+})
+    
+    document.querySelector(".btn2").addEventListener("click", function(){
+    document.querySelector(".popup2").style.display = "none"
+})
+
+    document.getElementById("view3").addEventListener("click", function(){
+    document.querySelector(".popup3").style.display = "flex"
+})
+        
+    document.querySelector(".btn3").addEventListener("click", function(){
+    document.querySelector(".popup3").style.display = "none"
+})
+
+
+
+
